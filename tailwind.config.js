32a9ee02b53049ea1cc731d74ec4be3a0a3c5d57
Leonardo Nicola <leonardonicolares@gmail.com>
2023-12-11 02:29:41 -0300

    feat: create pallete and add serif font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ export default {
   theme: {
     fontFamily: {
       sans: ['Inter', 'Arial', 'sans-serif'],
+      serif: ['Lora', 'Georgia', 'serif'],
+    },
+    extend: {
+      colors: {
+        background: '#F0EDEE',
+        primary: '#016FB9',
+        'dark-primary': '#084887',
+        'light-primary': '#23B5D3',
+        secondary: '#96C600',
+        'dark-secondary': '#5E7B00',
+        'light-secondary': '#E2ECC4',
+        success: '#028547',
+        error: '#E00E34',
+        info: '#3566FA',
+        atention: '#FFC24C',
+      },
     },
   },
   plugins: [],
