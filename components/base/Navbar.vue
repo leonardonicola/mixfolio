77f53bd2eedf9b27e3b6e2a3ee04e8a5cd21dc4c
Leonardo Nicola <leonardonicolares@gmail.com>
2023-12-11 22:59:16 -0300

    fix: mixfolio logo routing and button names

--- components/base/Navbar.vue
+++ components/base/Navbar.vue
@@ -2,10 +2,10 @@
 
 <template>
   <div class="w-screen px-8 md:px-16 h-24 flex items-center justify-between">
-    <RouterLink to="/" custom
-      ><h1 class="font-bold text-3xl font-serif cursor-pointer select-none">
-        Mixfolio
-      </h1></RouterLink
+    <RouterLink
+      to="/"
+      class="font-bold text-3xl font-serif select-none hover:text-primary transition-colors"
+      >Mixfolio</RouterLink
     >
 
     <div class="flex gap-5 items-center justify-between">
@@ -16,10 +16,10 @@
         >Home</RouterLink
       >
       <RouterLink
-        to="/sobre"
+        to="/favoritos"
         exact-active-class="border-b-4 border-b-primary"
         class="hover:cursor-pointer hover:text-primary transition-colors"
-        >Sobre</RouterLink
+        >Favoritos</RouterLink
       >
     </div>
   </div>
