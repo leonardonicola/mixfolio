f7b3a02ad4ae24470b9097ce90aecf4ebcb4346c
Leonardo Nicola <leonardonicolares@gmail.com>
2023-12-11 22:58:44 -0300

    feat: loading states

---
+++ components/Skeleton.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div
+      v-for="index in 6"
+      :key="index"
+      class="h-96 w-full animate-pulse bg-neutral-400 rounded-xl"
+    ></div>
+  </section>
+</template>
