40eb32b71caea3a6514f73f71489c2d88cdbd0c0
Leonardo Nicola <leonardonicolares@gmail.com>
2023-12-12 13:30:16 -0300

    test: add data-cy for easier cypress queries

--- components/base/Select.vue
+++ components/base/Select.vue
@@ -12,7 +12,7 @@ const emit = defineEmits(['optionSelected'])
 watch(selected, (newVal) => emit('optionSelected', newVal))
 </script>
 <template>
-  <select v-model="selected" class="select">
+  <select v-model="selected" class="select" data-cy="select">
     <option selected :value="null">{{ title }}</option>
     <option v-for="option in options" :key="option" :value="option">
       {{ option }}
